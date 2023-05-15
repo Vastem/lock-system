3a3243c5e359ca81bfc826d8a696a81a2866ba5f
Jorge Alan Retamoza Flores <102197374+Vastem@users.noreply.github.com>
2023-05-14 22:07:11 -0700

    allow cors origin

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.datos_route import datos
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permitir el acceso desde cualquier origen
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(datos)
